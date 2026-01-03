002e09f650730aca7fd1743d0bb8ed4387475ea7
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-01-04 00:33:32 +0100

    changeup githooks

--- modules/flake/git-hooks.nix
+++ modules/flake/git-hooks.nix
@@ -7,8 +7,8 @@
     pre-commit = {
       settings.hooks = {
         alejandra.enable = true;
-        deadnix.enable = true;
         statix.enable = true;
+        flake-checker.enable = true;
       };
     };
   };
