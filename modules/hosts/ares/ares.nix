8930ac4f7d6ed4e1f58d7c89d38ab44230482cb6
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-01-04 12:32:15 +0100

    add ares host

---
+++ modules/hosts/ares/ares.nix
@@ -0,0 +1,56 @@
+{inputs, ...}: {
+  flake.nixosConfigurations.ares = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      inputs.self.modules.nixos.ares
+    ];
+  };
+
+  flake.modules.nixos.ares = {config, ...}: {
+    nixpkgs.hostPlatform = "x86_64-linux";
+    system.stateVersion = "25.05";
+    hardware.facter.reportPath = ./facter.json;
+    networking.hostName = "ares";
+
+    boot = {
+      # nixos-facter mislabels my ethernet device
+      extraModulePackages = [config.boot.kernelPackages.yt6801];
+      kernelModules = ["yt6801"];
+      kernelParams = [
+        "amd_pstate=active"
+      ];
+    };
+
+    services.power-profiles-daemon.enable = true;
+
+    # Boost startup speed
+    virtualisation.docker.enableOnBoot = false;
+    systemd.services.NetworkManager-wait-online.enable = false;
+
+    home-manager.sharedModules = [
+      {
+        wayland.windowManager.hyprland.settings = {
+          monitor = [
+            ",preferred,auto,1.6"
+          ];
+        };
+      }
+    ];
+
+    imports = with inputs.self.modules.nixos; [
+      locale
+      systemd-boot
+      tuigreet
+      sound
+      amd
+      ssd
+      zram
+      docker
+      desktop
+      homeManager
+      twingate
+      stylix
+      nix
+      julius
+    ];
+  };
+}
---
+++ modules/hosts/ares/fs.nix
@@ -0,0 +1,16 @@
+{
+  flake.modules.nixos.ares = {
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/981e2aed-b34b-436a-9bf8-8eba9e3e54b0";
+      fsType = "ext4";
+    };
+
+    boot.initrd.luks.devices."luks-8d1d18b4-3391-4b63-8437-8fc62437b026".device = "/dev/disk/by-uuid/8d1d18b4-3391-4b63-8437-8fc62437b026";
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/EE6D-31BE";
+      fsType = "vfat";
+      options = ["fmask=0077" "dmask=0077"];
+    };
+  };
+}
