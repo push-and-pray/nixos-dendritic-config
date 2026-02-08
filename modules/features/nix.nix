4267983bbd7b72c648ac134905b571176fb94efe
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-02-08 13:04:55 +0100

    fix: set nh path correctly

--- modules/features/nix.nix
+++ modules/features/nix.nix
@@ -32,7 +32,7 @@
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep-since 4d --keep 3";
-      flake = "~/dev/nix/os-d";
+      flake = "~/dev/nix/os/";
     };
   };
 }
