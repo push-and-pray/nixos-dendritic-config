72a74f8fd4f37908d410b50acf215d68287b2b98
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-02-18 11:21:40 +0100

    feat: greetd remembers user and session

--- modules/features/display-managers.nix
+++ modules/features/display-managers.nix
@@ -5,7 +5,7 @@
         enable = true;
         settings = {
           default_session = {
-            command = "${pkgs.tuigreet}/bin/tuigreet --time";
+            command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session";
             user = "greeter";
           };
         };
