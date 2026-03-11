2a58c19ee8ec299a9dddc4c51205fe8b1d076e20
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-03-11 18:52:24 +0100

    feat: add vesktop as discord alternative

--- modules/features/desktop-apps.nix
+++ modules/features/desktop-apps.nix
@@ -47,7 +47,7 @@
       home.packages = with pkgs; [signal-desktop];
     };
     discord = {pkgs, ...}: {
-      home.packages = with pkgs; [discord];
+      home.packages = with pkgs; [vesktop discord];
     };
     surfer = {pkgs, ...}: {
       home.packages = with pkgs; [surfer];
