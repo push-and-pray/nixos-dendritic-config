8aacf30231914fafe3d4cf0ba47f32764410ae44
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-03-28 22:30:35 +0100

    feat: move to podman

--- modules/features/docker.nix
+++ modules/features/docker.nix
@@ -1,5 +1,26 @@
 {
-  flake.modules.nixos.docker = {
-    virtualisation.docker.enable = true;
+  flake.modules.nixos.docker = {pkgs, ...}: {
+    virtualisation = {
+      containers.enable = true;
+      podman = {
+        enable = true;
+        dockerCompat = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+    };
+
+    users.users.julius = {
+      extraGroups = [
+        "podman"
+      ];
+    };
+
+    home-manager.sharedModules = [
+      {
+        home.packages = with pkgs; [
+          podman-bootc
+        ];
+      }
+    ];
   };
 }
