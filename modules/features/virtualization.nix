a0584af75379200a3b11af1a9c037994657e8ad4
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-03-28 14:31:11 +0100

    feat: improve virt

--- modules/features/virtualization.nix
+++ modules/features/virtualization.nix
@@ -8,11 +8,21 @@
           swtpm.enable = true;
         };
       };
+
+      virtualisation.spiceUSBRedirection.enable = true;
       programs.virt-manager.enable = true;
 
       users.users.julius = {
         extraGroups = ["libvirtd"];
       };
+
+      home-manager.sharedModules = [
+        {
+          home.packages = with pkgs; [
+            remmina
+          ];
+        }
+      ];
     };
   };
 }
