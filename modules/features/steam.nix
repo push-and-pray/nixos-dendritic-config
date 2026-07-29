5b62848f20c439eb77367ab582055a913f8e5d38
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-07-29 19:48:16 +0200

    optimize steam

--- modules/features/steam.nix
+++ modules/features/steam.nix
@@ -1,20 +1,37 @@
 _: {
   flake.modules.nixos.steam = {pkgs, ...}: {
-    programs.steam = {
-      enable = true;
-
-      remotePlay.openFirewall = true;
-
-      dedicatedServer.openFirewall = true;
-
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
+    programs = {
+      gamemode = {
+        enable = true;
+        settings = {
+          general = {renice = 10;};
+          gpu = {
+            apply_gpu_optimisations = "accept-responsibility";
+            gpu_device = 0;
+            nv_powermizer_mode = 1;
+          };
+        };
+      };
+      gamescope = {
+        capSysNice = false;
+        enable = true;
+      };
+      steam = {
+        dedicatedServer = {openFirewall = true;};
+        enable = true;
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
+        gamescopeSession = {
+          args = ["-O" "DP-1"];
+          enable = true;
+        };
+        remotePlay = {openFirewall = true;};
+      };
     };
 
-    programs.gamescope = {
-      enable = true;
-      capSysNice = false;
-    };
+    environment.systemPackages = with pkgs; [
+      mangohud
+    ];
   };
 }
