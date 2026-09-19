50b1faa1f871cab025df5d6495ab6fc3320a69b0
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-09-19 21:50:01 +0200

    add atlas host

---
+++ modules/hosts/atlas/atlas.nix
@@ -0,0 +1,56 @@
+{ inputs, ... }: {
+  flake.nixosConfigurations.atlas = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      inputs.self.modules.nixos.atlas
+      inputs.sops-nix.nixosModules.sops
+    ];
+  };
+
+  flake.modules.nixos.atlas = { config, ... }: {
+    nixpkgs.hostPlatform = "x86_64-linux";
+    system.stateVersion = "26.11";
+    hardware.facter.reportPath = ./facter.json;
+
+    boot.loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    networking.hostName = "atlas";
+    networking.useDHCP = true;
+
+    environment.enableAllTerminfo = true;
+
+    security.sudo.wheelNeedsPassword = false;
+    users.users.julius = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPiaayaWR+KgD/2gUke5ll5ZKHMLnTJx/3bfc2522qiQ julius@ares"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIL27EDkViSsAa6PByx7ZaqAg2CgL3V1Wiy6RmQ/StegbAAAABHNzaDo= julius@zeus"
+      ];
+    };
+
+    services.openssh.settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+    };
+    services.tailscale = {
+      authKeyFile = config.sops.secrets.ts-key-atlas.path;
+      openFirewall = true;
+    };
+
+    sops.secrets.ts-key-atlas = {
+      sopsFile = ../../../secrets/ts-key.yaml;
+    };
+
+    imports = with inputs.self.modules.nixos; [
+      nix
+      locale
+      ssh
+      tailscale
+      sops
+    ];
+  };
+}
---
+++ modules/hosts/atlas/disko.nix
@@ -0,0 +1,33 @@
+{ inputs, ... }: {
+  flake.modules.nixos.atlas = {
+    imports = [ inputs.disko.nixosModules.disko ];
+
+    disko.devices.disk.main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
+}
